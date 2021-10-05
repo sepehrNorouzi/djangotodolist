ebce8c2cf7fdc75138d49a9006fe1d7a1216a251
Sepehr Norouzi <sepehr.norouzi1999@gmail.com>
2021-10-06 00:06:17 +0330

    Version 1.0.1 User Model imported. Signup form imported. Signup view impelemented. Users can now be created & saved into the DB. Objective: Error Handeling

--- todolist-project/todo/views.py
+++ todolist-project/todo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+def signupuser(request):
+    if request.method == "GET":
+        return render(request, 'todo/signupuser.html', {'signupform': UserCreationForm()})
+    else:
+        postObj = request.POST
+        if postObj['password1'] == postObj['password2']:
+            user = User.objects.create_user(postObj['username'], password=postObj['password1'])
+            user.save()
+        else:
+            pass
