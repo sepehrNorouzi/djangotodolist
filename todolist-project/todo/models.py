a496ebe55db3f35a0640c1fbedbd3ff96db0dd1d
Sepehr Norouzi <sepehr.norouzi1999@gmail.com>
2021-10-07 16:18:06 +0330

    Version 1.1.1 Todo model has been added.

--- todolist-project/todo/models.py
+++ todolist-project/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(null=False, max_length=30, blank=False)
+    detail = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_important = models.BooleanField(default=False)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self) -> str:
+        return self.title + ' - ' + self.user.username
